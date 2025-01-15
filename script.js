535537bc5fc4cbfb6adfe1e22be3abf85eb8a600
Hemel <rh189827@gmail.com>
2025-01-15 19:06:39 +0600

    Badge Section Added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//Toggle Bar//
-const navLinks = document.querySelector('.nav-links'); // Fixed variable name
+//Toggle Bar
+const navLinks = document.querySelector('.nav-links'); 
 function ontoggleMenu(e) {
     e.name = e.name === 'menu' ? 'close' : 'menu';
-    navLinks.classList.toggle('top-[5%]'); // Corrected variable and class name
+    navLinks.classList.toggle('top-[5%]');
 }

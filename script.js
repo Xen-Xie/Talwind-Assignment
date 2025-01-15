668edb20f22ea9913e24c89cdc93ea6bccb32873
Hemel <rh189827@gmail.com>
2025-01-16 01:04:13 +0600

    dark mode enable option added

--- script.js
+++
@@ -1,6 +0,0 @@
-//Toggle Bar
-const navLinks = document.querySelector('.nav-links'); 
-function ontoggleMenu(e) {
-    e.name = e.name === 'menu' ? 'close' : 'menu';
-    navLinks.classList.toggle('top-[5%]');
-}
